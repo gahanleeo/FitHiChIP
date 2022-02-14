21e45222394865347bcab4d7d396d3dd2490c672
gahanleeo <48963946+gahanleeo@users.noreply.github.com>
2022-02-14 13:54:37 -0500

    Update DiffAnalysisHiChIP.r

--- Imp_Scripts/DiffAnalysisHiChIP.r
+++ Imp_Scripts/DiffAnalysisHiChIP.r
@@ -822,14 +822,12 @@ for (i in (1:length(ChIPAlignFileList))) {
 		#}
 		# use bedmap, swith NAN to 0, get the mean 
 		if (i == 1){
-			system(paste("bedmap --echo --mean", TargetBinnedChrFile,ChIPAlignFileList[i], ">", "pre_merged_file"))
-			system(paste("awk '{sub(/\|/,"\t"); print $0}'","pre_merged_file",">", MergedChIPCovFile)) # seperate column from "|"
+			system(paste("bedmap --echo --mean", TargetBinnedChrFile , ChIPAlignFileList[i], "| awk","'{sub(/\\|/,\"\t\"); print $0}'",">", MergedChIPCovFile )) #seperate column by "|"
 		} else {
 			system(paste("bedmap --mean", TargetBinnedChrFile,ChIPAlignFileList[i], ">", tempfile1 ))
-			system(paste("paste", MergedChIPCovFile, tempfile1, ">", tempfile2))
-			# replace NAN to 0
+			system(paste("paste", MergedChIPCovFile, tempfile1, "| sed 's/NAN/0/g'", ">", tempfile2)) # replace NAN to 0
 			system(paste("mv", tempfile2, MergedChIPCovFile))
-		
+		}
 }
 
 # end input file loop
